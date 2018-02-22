78aa07a814ee733218297ff952301703c3865081
David Paulsson <david@davidpaulsson.se>
2018-02-22 08:54:35 +0100

    Remove falsey values from data source

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View, SectionList } from 'react-native';
 import _ from 'lodash';
-import data from './fixtures/data.json';
+import json from './fixtures/data.json';
 
 const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ';
 export default class App extends React.Component {
   state = {
-    dataSource: [...alphabet].map(letter => ({
-      data: data.filter(d => d.name.toUpperCase().startsWith(letter)),
-      title: letter,
-    })),
+    dataSource: _.compact(
+      [...alphabet].map(letter => {
+        const data = json.filter(d => d.name.toUpperCase().startsWith(letter));
+        if (data.length === 0) {
+          return false;
+        }
+        return {
+          data,
+          title: letter,
+        };
+      })
+    ),
   };
   keyExtractor = (item, index) => item.id;
   render() {
