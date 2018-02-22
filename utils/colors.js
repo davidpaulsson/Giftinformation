f1345fa9c8eb09ca119d7a7caadf525696d4d2c5
David Paulsson <david@davidpaulsson.se>
2018-02-22 09:01:04 +0100

    Add colors

---
+++ utils/colors.js
@@ -0,0 +1,11 @@
+export default {
+  sunflowerYellow: '#ffde00',
+  highlighterGreen: '#2fe900',
+  darkGreenBlue: '#2f7a5e',
+  brightYellow: '#fff100',
+  white: '#d4d4d4',
+  red: '#ff0000',
+  brightBlue: '#0076ff',
+  brightOrange: '#ff6d00',
+  greyishBrown: '#3f3f3f',
+};
