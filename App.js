10f34c38a300db07814ef637bb928cb1cae0262a
David Paulsson <david@davidpaulsson.se>
2018-11-06 18:46:43 +0100

    Upgrade deprecated syntax

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { ScreenOrientation } from 'expo';
-import { StackNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation';
 import HomeScreen from './screens/HomeScreen';
 import DetailsScreen from './screens/DetailsScreen';
 
-ScreenOrientation.allow(ScreenOrientation.Orientation.PORTRAIT_UP);
+ScreenOrientation.allowAsync(ScreenOrientation.Orientation.PORTRAIT_UP);
 
-export default StackNavigator(
+export default createStackNavigator(
   {
     Home: {
       screen: HomeScreen,
